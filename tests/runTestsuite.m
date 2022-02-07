45e449eb510856f298ff6c9d0541f5d99ac19ec9
promero <promero@mathworks.com>
2022-02-07 08:30:40 +0100

    Enable filter test cases to run with tags as argument.

--- tests/runTestsuite.m
+++ tests/runTestsuite.m
@@ -1,14 +1,22 @@
-function runTestsuite
+function runTestsuite(tags)
+
+arguments
+    tags {mustBeText} = '';
+end
 
 import matlab.unittest.TestRunner;
 import matlab.unittest.TestSuite;
 import matlab.unittest.Verbosity;
 import matlab.unittest.plugins.XMLPlugin;
 import matlab.unittest.plugins.TestReportPlugin;
+import matlab.unittest.selectors.HasTag;
 
 projObj = currentProject;
 
 suite = TestSuite.fromProject(projObj);
+if ~isempty(tags)
+    suite = suite.selectIf(HasTag(tags));
+end
 
 runner = TestRunner.withTextOutput('OutputDetail', Verbosity.Detailed);
 runner.addPlugin(XMLPlugin.producingJUnitFormat('results.xml'));
